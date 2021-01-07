ad0a00094f289d7ce59f671bc6b44c84e8d3055b
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-07 06:37:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,7 +18,7 @@ def game_hash
           :slam_dunks => 1,
         },
         {
-          :player_name => "Reggie Evans"
+          :player_name => "Reggie Evans",
           :number => 30,
           :shoe => 14,
           :points => 12,
@@ -29,7 +29,7 @@ def game_hash
           :slam_dunks => 7,
         },
         {
-          :player_name => "Brook Lopez"
+          :player_name => "Brook Lopez",
           :number => 11,
           :shoe => 17,
           :points => 17,
@@ -40,7 +40,7 @@ def game_hash
           :slam_dunks => 15,
         },
         {
-          :player_name => "Mason Plumlee"
+          :player_name => "Mason Plumlee",
           :number => 1,
           :shoe => 19,
           :points => 26,
@@ -51,7 +51,7 @@ def game_hash
           :slam_dunks => 5,
         },
         {
-          :player_name => "Jason Terry"
+          :player_name => "Jason Terry",
           :number => 31,
           :shoe => 15,
           :points => 19,
@@ -67,7 +67,7 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise","Purple"],
       :players => [
-        {:player_name => "Jeff Adrien"
+        {:player_name => "Jeff Adrien",
           :number => 4
           :shoe => 18
           :points => 10
