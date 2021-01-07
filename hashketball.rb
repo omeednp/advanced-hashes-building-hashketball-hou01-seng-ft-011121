237b38a7157d6e632a665e0b74f6dcf50a120456
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-07 06:18:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,13 @@
 def game_hash
   hashketball = {
     :home => {
-      
+      :team_name => ""
+      :colors => ["",""]
+      :players => [
+        {:player_name},
+        {
+          
+        }]
     }
     
     
