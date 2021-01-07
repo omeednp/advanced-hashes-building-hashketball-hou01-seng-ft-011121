8e8f7b40d45f4c619ca63feb73ba034f22c83592
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-07 06:25:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ def game_hash
         {:player_name => "Alan Anderson", "Reggie Evans",	"Brook Lopez",	"Mason Plumlee",	"Jason Terry"},
         {:number => 0,	30,	11,	1,	31},
         {:shoe => 16,	14,	17,	19,	15,},
-        {:points => },
+        {:points => 22,	12,	17,	26,	19},
         {:rebounds => },
         {:assists => },
         {:steals => },
