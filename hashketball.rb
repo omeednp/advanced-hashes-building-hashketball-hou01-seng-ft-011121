7caf7480ab093876e3f1fd5f356db4af171d44da
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-07 06:44:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 def game_hash
-  
+  require 'pry'
   
   hashketball = {
     :home => {
@@ -96,6 +96,7 @@ def game_hash
           :steals => 4,
           :blocks => 5,
           :slam_dunks => 5,
+          binding.pry
         },
         { :player_name => "Ben Gordon",
           :number => 8,
