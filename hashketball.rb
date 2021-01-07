9459fa5a55ee14e480d9311999bbaf7d810e9ac6
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-07 06:25:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ def game_hash
         {:shoe => 16,	14,	17,	19,	15,},
         {:points => 22,	12,	17,	26,	19},
         {:rebounds => 12,	12,	19,	11,	2},
-        {:assists => },
+        {:assists => 12,	12,	10,	6,	2},
         {:steals => },
         {:blocks => },
         {:slam_dunks => }
